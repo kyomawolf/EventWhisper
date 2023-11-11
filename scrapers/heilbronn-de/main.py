1282848e6d79b6d8cfc86971ed25ff0eaaa20bac
hmnd <github@hmnd.de>
2023-11-11 09:09:20 +0100

    change interests to gpt4

--- scrapers/heilbronn-de/main.py
+++ scrapers/heilbronn-de/main.py
@@ -119,7 +119,7 @@ def load_interests(event):
     client = OpenAI()
 
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo-1106",
+        model="gpt-4-1106-preview",
         response_format={"type": "json_object"},
         messages=[
             {
